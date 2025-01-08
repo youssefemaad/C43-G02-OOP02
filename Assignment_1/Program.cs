6474b0c25f3ac4a016249d3d5f7b9e4b6d628318
Youssef Emad <eyoussef228@gmail.com>
2025-01-08 20:51:08 +0200

    new

--- Assignment_1/Program.cs
+++ Assignment_1/Program.cs
@@ -1,156 +1,136 @@
-﻿// Assignment 01 OOP
+﻿// Assignment 2 OOP
 using System;
 
-namespace Assignment01OOP
+namespace Assignment02OOP
 {
-    public enum WeekDays
-    {
-        Monday,
-        Tuesday,
-        Wednesday,
-        Thursday,
-        Friday,
-        Saturday,
-        Sunday
-    }
-
-    public enum Season
-    {
-        Spring,
-        Summer,
-        Autumn,
-        Winter
-    }
-
-    public enum Permissions
-    {
-        Read = 1,
-        Write = 2,
-        Delete = 4,
-        Execute = 8
-    }
-
-    public enum Colors
-    {
-        Red,
-        Green,
-        Blue
-    }
-
     class Program
     {
+        public struct Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        public struct Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+        }
+
+        public struct Rectangle
+        {
+            private double width;
+            private double height;
+
+            public double Width
+            {
+                get { return width; }
+                set { width = value; }
+            }
+
+            public double Height
+            {
+                get { return height; }
+                set { height = value; }
+            }
+
+            public double Area()
+            {
+                return width * height;
+            }
+
+            public void Display()
+            {
+                Console.WriteLine($"Width: {width}, Height: {height}, Area: {Area()}");
+            }
+        }
+
         static void Main(string[] args)
         {
-            #region 1. Create an Enum called "WeekDays" with the days of the week (Monday to Sunday) as its members.
-            // Write a C# program that prints out all the days of the week using this Enum.
-            Console.WriteLine("Days of the week:");
-            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            #region 1. Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+            Person[] persons = new Person[3];
+            for (int i = 0; i < persons.Length; i++)
             {
-                Console.WriteLine(day);
+                Console.Write("Enter name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter age: ");
+                int age = int.Parse(Console.ReadLine());
+                persons[i] = new Person { Name = name, Age = age };
+            }
+
+            foreach (Person person in persons)
+            {
+                Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
             }
             
             #endregion
 
-            #region 2. Create an Enum called "Season" with the four seasons (Spring, Summer, Autumn, Winter) as its members.
-            // Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season.
-            Console.WriteLine("Enter a season:");
-            string season = Console.ReadLine();
-            if (Enum.TryParse<Season>(season, out Season result))
-            {
-                if (result == Season.Spring)
-                {
-                    Console.WriteLine("Spring: March, April, May");
-                }
-                else if (result == Season.Summer)
-                {
-                    Console.WriteLine("Summer: June, July, August");
-                }
-                else if (result == Season.Autumn)
-                {
-                    Console.WriteLine("Autumn: September, October, November");
-                }
-                else if (result == Season.Winter)
-                {
-                    Console.WriteLine("Winter: December, January, February");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid season");
-            }
+            #region 2. Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+            Point point1 = new Point();
+            Point point2 = new Point();
 
+            Console.Write("Enter X1: ");
+            point1.X = double.Parse(Console.ReadLine());
+            Console.Write("Enter Y1: ");
+            point1.Y = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter X2: ");
+            point2.X = double.Parse(Console.ReadLine());
+            Console.Write("Enter Y2: ");
+            point2.Y = double.Parse(Console.ReadLine());
+
+            double distance = Math.Sqrt(Math.Pow(point2.X - point1.X, 2) + Math.Pow(point2.Y - point1.Y, 2));
+            Console.WriteLine($"Distance between the two points: {distance}");
+            
             #endregion
 
-            #region 3. Assign the following Permissions (Read, Write, Delete, Execute) in a form of Enum.
-            // Create a variable from the previous Enum to Add and Remove Permission from the variable.
-            // Check if a specific Permission exists inside the variable.
-            Permissions permissions = Permissions.Read | Permissions.Write;
-            Console.WriteLine("Permissions: " + permissions);
+            #region 3. Create a struct called "Person" with properties "Name" and "Age". Write a C# program that takes details of 3 persons as input from the user and displays the name and age of the oldest person.
+            Person person1 = new Person();
+            Person person2 = new Person();
+            Person person3 = new Person();
 
-            permissions |= Permissions.Delete;
-            Console.WriteLine("Permissions: " + permissions);
+            Console.Write("Enter name of person 1: ");
+            person1.Name = Console.ReadLine();
+            Console.Write("Enter age of person 1: ");
+            person1.Age = int.Parse(Console.ReadLine());
 
-            permissions &= ~Permissions.Write;
-            Console.WriteLine("Permissions: " + permissions);
+            Console.Write("Enter name of person 2: ");
+            person2.Name = Console.ReadLine();
+            Console.Write("Enter age of person 2: ");
+            person2.Age = int.Parse(Console.ReadLine());
 
-            if ((permissions & Permissions.Read) == Permissions.Read)
-            {
-                Console.WriteLine("Read permission exists");
-            }
-            else
-            {
-                Console.WriteLine("Read permission does not exist");
-            }
+            Console.Write("Enter name of person 3: ");
+            person3.Name = Console.ReadLine();
+            Console.Write("Enter age of person 3: ");
+            person3.Age = int.Parse(Console.ReadLine());
 
-            if ((permissions & Permissions.Write) == Permissions.Write)
+            Person oldestPerson = person1;
+
+            if (person2.Age > oldestPerson.Age)
             {
-                Console.WriteLine("Write permission exists");
-            }
-            else
-            {
-                Console.WriteLine("Write permission does not exist");
+                oldestPerson = person2;
             }
 
-            if ((permissions & Permissions.Delete) == Permissions.Delete)
+            if (person3.Age > oldestPerson.Age)
             {
-                Console.WriteLine("Delete permission exists");
-            }
-            else
-            {
-                Console.WriteLine("Delete permission does not exist");
+                oldestPerson = person3;
             }
 
-            if ((permissions & Permissions.Execute) == Permissions.Execute)
-            {
-                Console.WriteLine("Execute permission exists");
-            }
-            else
-            {
-                Console.WriteLine("Execute permission does not exist");
-            }
-
+            Console.WriteLine($"The oldest person is {oldestPerson.Name} with age {oldestPerson.Age}");
+            
             #endregion
 
-            #region 4. Create an Enum called "Colors" with the basic colors (Red, Green, Blue) as its members.
-            // Write a C# program that takes a color name as input from the user and displays a message
-            // indicating whether the input color is a primary color or not.
-            Console.WriteLine("Enter a color:");
-            string color = Console.ReadLine();
-            if (Enum.TryParse<Colors>(color, out Colors resultColor))
-            {
-                if (resultColor == Colors.Red || resultColor == Colors.Green || resultColor == Colors.Blue)
-                {
-                    Console.WriteLine("Primary color");
-                }
-                else
-                {
-                    Console.WriteLine("Not a primary color");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid color");
-            }
+            #region 4. Create a struct named Rectangle that represents a rectangle with encapsulated fields "width" and "height". Implement properties and methods to calculate area and display details.
+            Rectangle rectangle = new Rectangle();
+            Console.Write("Enter width: ");
+            rectangle.Width = double.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            rectangle.Height = double.Parse(Console.ReadLine());
+
+            double area = rectangle.Area();
+            Console.WriteLine($"Area of the rectangle: {area}");
+
+            rectangle.Display();
             
             #endregion
         }
